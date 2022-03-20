8a5c0308947d45bd5ff389d409cb38873110c178
SantiGonzalezLago <santiago.gonzalez.lago@gmail.com>
2022-03-20 14:56:50 +0100

    Corregida página de administración

--- app/Views/admin.php
+++ app/Views/admin.php
@@ -16,41 +16,41 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ?>
 
-<div class="d-flex align-items-start pt-3">
-  <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-    <button class="nav-link active" id="settings-tab" data-bs-toggle="pill" data-bs-target="#settings" type="button" role="tab" aria-controls="settings" aria-selected="true">
-        Configuración
-    </button>
-    <button class="nav-link" id="users-tab" data-bs-toggle="pill" data-bs-target="#users" type="button" role="tab" aria-controls="users" aria-selected="false">
-        Administrar usuarios
-    </button>
-  </div>
-  <div class="tab-content" id="v-pills-tabContent">
-    <div class="tab-pane fade show active" id="settings" role="tabpanel" aria-labelledby="settings-tab">
-        <table class="table">
-            <?php foreach ($settings as $setting) : ?>
-                <tr class="setting-row">
-                    <th data-bs-toggle="tooltip" data-bs-original-title="<?= $setting['description'] ?>">
-                        <label for="<?= $setting['key'] ?>"><?= $setting['key'] ?></label>
-                    </th>
-                    <td>
-                        <?php if ($setting['type'] != "checkbox") : ?>
-                            <input type="<?= $setting['type'] ?>" name="<?= $setting['key'] ?>" id="<?= $setting['key'] ?>" value="<?= $setting['value'] ?>" class="form-control">
-                        <?php else: ?>
-                            <input type="checkbox" name="<?= $setting['key'] ?>" id="<?= $setting['key'] ?>" value="1" <?= $setting['value'] ? "checked" : "" ?> >
-                        <?php endif; ?>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-            <button type="button" id="save-settings" class="btn btn-primary" disabled>Guardar</button>
+<div class="align-items-start pt-3">
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#settings" role="tab" aria-controls="home" aria-selected="true">Configuración</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#users" role="tab" aria-controls="profile" aria-selected="false">Administrar usuarios</a>
+        </li>
+    </ul>
+    <div class="tab-content" id="v-pills-tabContent">
+        <div class="tab-pane fade show active" id="settings" role="tabpanel" aria-labelledby="settings-tab">
+            <table class="table table-borderless">
+                <?php foreach ($settings as $setting) : ?>
+                    <tr class="setting-row">
+                        <th class="text-center w-25" data-toggle="tooltip" data-original-title="<?= $setting['description'] ?>">
+                            <label for="<?= $setting['key'] ?>"><?= $setting['key'] ?></label>
+                        </th>
+                        <td>
+                            <?php if ($setting['type'] != "checkbox") : ?>
+                                <input type="<?= $setting['type'] ?>" name="<?= $setting['key'] ?>" id="<?= $setting['key'] ?>" value="<?= $setting['value'] ?>" class="form-control">
+                            <?php else: ?>
+                                <input type="checkbox" name="<?= $setting['key'] ?>" id="<?= $setting['key'] ?>" value="1" <?= $setting['value'] ? "checked" : "" ?> >
+                            <?php endif; ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button type="button" id="save-settings" class="btn btn-primary" disabled>Guardar</button>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="users" role="tabpanel" aria-labelledby="users-tab">
+            Administrar usuarios
         </div>
     </div>
-    <div class="tab-pane fade" id="users" role="tabpanel" aria-labelledby="users-tab">
-        Administrar usuarios
-    </div>
-  </div>
 </div>
 
 <div class="modal" id="save-settings-modal" tabindex="-1">
@@ -60,7 +60,7 @@
                 <h5 class="modal-title">Se han guardado los cambios.</h5>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
             </div>
         </div>
     </div>
