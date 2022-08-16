a4f16b3641020c4b4254a543a082014e480eecbb
SantiGonzalezLago <santiago.gonzalez.lago@gmail.com>
2022-08-16 23:13:51 +0200

    base_url

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/warhammer_elo_webapp/public/';
+    public $baseURL = 'http://localhost:8080/';
 
     /**
      * --------------------------------------------------------------------------
